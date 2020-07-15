83f4babc925dcbcc11f8f21786f831e32646dbde
huntercront <38252870+huntercront@users.noreply.github.com>
2020-07-15 21:26:34 +0300

    add form

---
+++ js/script.js
@@ -0,0 +1,32 @@
+function getScrollbarWidth() {
+  const outer = document.createElement('div');
+  outer.style.visibility = 'hidden';
+  outer.style.overflow = 'scroll';
+  outer.style.msOverflowStyle = 'scrollbar'; 
+  document.body.appendChild(outer);
+  const inner = document.createElement('div');
+  outer.appendChild(inner);
+  const scrollbarWidth = (outer.offsetWidth - inner.offsetWidth);
+  outer.parentNode.removeChild(outer);
+  return scrollbarWidth;
+}
+
+console.log(document.body.clientHeight)
+console.log(window.innerHeight)
+var modal = document.querySelector('.form-modal');
+var contuctUs = document.querySelector('.contuct-us');
+var closeModal = document.querySelector('.close-form-button');
+contuctUs.addEventListener('click',function(e){
+	modal.classList.add('visible');
+	if(document.body.clientHeight > window.innerHeight){
+		document.body.style.overflow = 'hidden';
+		document.body.style.paddingRight = getScrollbarWidth()+'px';
+	}
+})
+closeModal.addEventListener('click',function(e){
+	modal.classList.remove('visible');
+	if(document.body.clientHeight > window.innerHeight){
+		document.body.style.overflow = 'auto';
+		document.body.style.paddingRight = 0+'px';
+	}
+})
